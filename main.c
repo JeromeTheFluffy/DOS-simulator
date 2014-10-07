280ff4a8362e44360677ca6c8f3ac8a373ca89ed
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2014-10-07 16:31:59 -0600

    main program
    main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+/*Preprocessor commands*/
+string commandString;
+printf("enter command here:");
+scanf(%d,commandString);
+int numOfChars;
+numOfChars=sizeof(commandString);
+typedef struct{;
+	/*enter more variables here*/
+	char command[numOfChars];
+};
